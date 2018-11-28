fe41fc2b9a4c84a0426c77d8472873c4ee27b8a5
Pawel Siemienik <p.siemienik@ohgames.net>
2018-11-28 21:47:48 +0100

    build

--- spec/config/setup-unit.js
+++
@@ -1,5 +0,0 @@
-// must configure PromishLib for unit tests
-const Promish = require('promish');
-const PromishLib = require('../../lib/utils/promish');
-
-PromishLib.Promish = Promish;
--- spec/config/setup.js
+++
@@ -1,9 +0,0 @@
-var chai = require('chai');
-
-var chaiXml = require('chai-xml');
-var chaiDatetime = require('chai-datetime');
-var dirtyChai = require('dirty-chai');
-
-chai.use(chaiXml);
-chai.use(chaiDatetime);
-chai.use(dirtyChai);
